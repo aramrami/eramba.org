33a1d1e1b89609eac11beab5623ba862df13b3b6
Martin Marko <martin.marko@ideapp.sk>
2017-12-08 14:12:15 +0100

    Consulting services subpage

---
+++ wp-content/themes/e.org/page-templates/consulting-services.php
@@ -0,0 +1,197 @@
+<?php // Template Name: Consulting Services ?>
+<?php
+get_header();
+?>
+
+<div class="jumbo-box light-blue">
+	<div class="container">
+		<div class="row align-row vertical-align-row">
+			<div class="col-sm-8">
+				<h3 class="margin-top-10">
+					<strong>Get help from the core team</strong>
+				</h3>
+				<p class="partnerships-text">
+					We offer a packaged yearly support service that includes installation, training, immediate patch resolutions and features not necessarily published on the community version at extremely affordable prices. With the help of our global partners, we also provide customisation and consulting services.
+				</p>
+				<p class="partnerships-text small">
+                    The income generated from these services is what keeps this project alive. We re-invest %100 into the project and we have no intention to become yet another GRC company.
+				</p>
+			</div>
+			<div class="col-sm-4 text-center img-wrapper padding-top-40	padding-bottom-40">
+				<img src="<?php echo do_shortcode('[img]'); ?>gears.png" alt="">
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container padding-top-30">
+	<div class="row vertical-align-row consulting-services-line">
+        <div class="col-sm-3">
+            <div class="doc-box">
+                <div class="doc-box-img">
+                    <img src="<?php echo do_shortcode('[img]'); ?>enterprise-icons/upgrades.png" alt="" style="margin-top:5px;">
+                </div>
+                <div class="doc-box-content equal">
+                    <h4>Remote Ciso</h4>
+                    <p>Over 35 thousand people visits eramba every year - a partner portal will show your company logo and website.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-9 consulting-services-top-line">
+            <div class="row align-row">
+                <div class="col-sm-12">
+                    <div class="doc-box align-col">
+        				<div class="doc-box-img">
+        					<img src="<?php echo do_shortcode('[img]'); ?>enterprise-icons/install.png" alt="" style="margin-top:5px;">
+        				</div>
+        				<div class="doc-box-content equal">
+        					<h4>Install Services</h4>
+        					<p>You and your colleagues can access our enterprise trainings at no cost.</p>
+        				</div>
+        			</div>
+                </div>
+            </div>
+
+            <div class="row align-row">
+                <div class="col-sm-4">
+                    <div class="doc-box align-col">
+                        <div class="doc-box-img">
+                            <img src="<?php echo do_shortcode('[img]'); ?>enterprise-icons/training.png" alt="" style="margin-top:5px;">
+                        </div>
+                        <div class="doc-box-content equal">
+                            <h4>On site trainings</h4>
+                            <p>Over 35 thousand people visits eramba every year - a partner portal will show your company logo.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-4">
+                    <div class="doc-box align-col">
+                        <div class="doc-box-img">
+                            <img src="<?php echo do_shortcode('[img]'); ?>enterprise-icons/training.png" alt="" style="margin-top:5px;">
+                        </div>
+                        <div class="doc-box-content equal">
+                            <h4>Remote trainings</h4>
+                            <p>Eramba every year - a partner portal will show your company logo and website.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-4">
+                    <div class="doc-box align-col">
+                        <div class="doc-box-img">
+                            <img src="<?php echo do_shortcode('[img]'); ?>enterprise-icons/import.png" alt="" style="margin-top:5px;">
+                        </div>
+                        <div class="doc-box-content equal">
+                            <h4>Import Data</h4>
+                            <p>Over 35 thousand people visits eramba every year - a partner portal will show your company logo and website.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row align-row">
+                <div class="col-sm-4">
+                    <div class="doc-box align-col">
+                        <div class="doc-box-img">
+                            <img src="<?php echo do_shortcode('[img]'); ?>doc/calendar.png" alt="" style="margin-top:5px;">
+                        </div>
+                        <div class="doc-box-content equal">
+                            <h4>Complience Management</h4>
+                            <p>Over 35 thousand people visits eramba every year - a partner portal will show your company.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-4">
+                    <div class="doc-box align-col">
+                        <div class="doc-box-img">
+                            <img src="<?php echo do_shortcode('[img]'); ?>doc/risk.png" alt="" style="margin-top:5px;">
+                        </div>
+                        <div class="doc-box-content equal">
+                            <h4>Risk Management</h4>
+                            <p>Every year - a partner portal will show your company logo and website.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-4">
+                    <div class="doc-box align-col">
+                        <div class="doc-box-img">
+                            <img src="<?php echo do_shortcode('[img]'); ?>enterprise-icons/data-flow.png" alt="" style="margin-top:5px;">
+                        </div>
+                        <div class="doc-box-content equal">
+                            <h4>Data Flow Analisis</h4>
+                            <p>Visits eramba every year - a partner portal will show your company logo and website.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row align-row no-bottom-margin">
+                <div class="col-sm-4">
+                    <div class="doc-box align-col">
+                        <div class="doc-box-img">
+                            <img src="<?php echo do_shortcode('[img]'); ?>doc/document.png" alt="" style="margin-top:5px;">
+                        </div>
+                        <div class="doc-box-content equal">
+                            <h4>Policy Reviews</h4>
+                            <p>Over 35 thousand people visits eramba every year - a partner portal will show your company logo and website.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-4">
+                    <div class="doc-box align-col">
+                        <div class="doc-box-img">
+                            <img src="<?php echo do_shortcode('[img]'); ?>enterprise-icons/box.png" alt="" style="margin-top:5px;">
+                        </div>
+                        <div class="doc-box-content equal">
+                            <h4>Control Testing</h4>
+                            <p>Over 35 thousand people visits eramba every year - a partner portal will show your company logo and website.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-4">
+                    <div class="doc-box align-col">
+                        <div class="doc-box-img">
+                            <img src="<?php echo do_shortcode('[img]'); ?>doc/suitcase.png" alt="" style="margin-top:5px;">
+                        </div>
+                        <div class="doc-box-content equal">
+                            <h4>Vendor Assesments</h4>
+                            <p>Over 35 thousand people visits eramba every year - a partner portal will show your company.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+	</div>
+
+    <div class="text-center margin-top-30 margin-bottom-60">
+		<a id="contact-redirect" href="#" class="btn btn-success btn-lg btn-wide">More information</a>
+	</div>
+</div>
+
+<script type="text/javascript">
+    $(function(){
+        function alignCols() {
+            $('.align-row').each(function() {
+                var height = 0;
+                console.log(height);
+                $(this).find('.align-col').each(function() {
+                    if ($(this).height() > height) {
+                        height = $(this).height();
+                    }
+                });
+                $(this).find('.align-col').height(height);
+            });
+        }
+
+        $(window).on("load", function($) {
+            alignCols();
+        });
+
+    });
+</script>
+<?php get_footer(); ?>
