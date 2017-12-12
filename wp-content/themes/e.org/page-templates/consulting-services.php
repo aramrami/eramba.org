cdff88be2d1db4ed881cacd2073770044aa93295
Martin Marko <martin.marko@ideapp.sk>
2017-12-12 10:26:22 +0100

    change for vertical "arrow" on subpage

--- wp-content/themes/e.org/page-templates/consulting-services.php
+++ wp-content/themes/e.org/page-templates/consulting-services.php
@@ -7,14 +7,11 @@ get_header();
 	<div class="container">
 		<div class="row align-row vertical-align-row">
 			<div class="col-sm-8">
-				<h3 class="margin-top-10">
-					<strong>Get help from the core team</strong>
-				</h3>
 				<p class="partnerships-text">
-					We offer a packaged yearly support service that includes installation, training, immediate patch resolutions and features not necessarily published on the community version at extremely affordable prices. With the help of our global partners, we also provide customisation and consulting services.
+				If your aim is to start using eramba as soon as possible and with the right foot, then you might want to consider our consulting services. Over the years we have helped many companies get started here in Europe and abroad, we have a tested methodology that works and is efficient.
 				</p>
-				<p class="partnerships-text small">
-                    The income generated from these services is what keeps this project alive. We re-invest %100 into the project and we have no intention to become yet another GRC company.
+				<p class="partnerships-text">
+				Our services are packaged (you know when they start, end and what you get) and work as much as possible at a fix price.
 				</p>
 			</div>
 			<div class="col-sm-4 text-center img-wrapper padding-top-40	padding-bottom-40">
@@ -25,19 +22,8 @@ get_header();
 </div>
 
 <div class="container padding-top-30">
-	<div class="row vertical-align-row consulting-services-line">
-        <div class="col-sm-3">
-            <div class="doc-box">
-                <div class="doc-box-img">
-                    <img src="<?php echo do_shortcode('[img]'); ?>enterprise-icons/upgrades.png" alt="" style="margin-top:5px;">
-                </div>
-                <div class="doc-box-content equal">
-                    <h4>Remote Ciso</h4>
-                    <p>Over 35 thousand people visits eramba every year - a partner portal will show your company logo and website.</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-9 consulting-services-top-line">
+	<div class="row consulting-services-row">
+        <div class="col-sm-12 consulting-services-top-box">
             <div class="row align-row">
                 <div class="col-sm-12">
                     <div class="doc-box align-col">
@@ -45,8 +31,13 @@ get_header();
         					<img src="<?php echo do_shortcode('[img]'); ?>enterprise-icons/install.png" alt="" style="margin-top:5px;">
         				</div>
         				<div class="doc-box-content equal">
-        					<h4>Install Services</h4>
-        					<p>You and your colleagues can access our enterprise trainings at no cost.</p>
+        					<h4><a href="">Install Services (Community Users)</a></h4>
+        					<p>For our community users we help you install eramba on your company or at the cloud for a fix price.</p>
+							<ul>
+								<li>Starting at $500</li>
+								<li>Onsite</li>
+								<li>Remote</li>
+							</ul>
         				</div>
         			</div>
                 </div>
@@ -60,7 +51,10 @@ get_header();
                         </div>
                         <div class="doc-box-content equal">
                             <h4>On site trainings</h4>
-                            <p>Over 35 thousand people visits eramba every year - a partner portal will show your company logo.</p>
+                            <p>If your team is larger than a couple of people we can travel onsite and deliver a in deep three day training.</p>
+							<ul>
+								<li>Onsite</li>
+							</ul>
                         </div>
                     </div>
                 </div>
@@ -71,8 +65,12 @@ get_header();
                             <img src="<?php echo do_shortcode('[img]'); ?>enterprise-icons/training.png" alt="" style="margin-top:5px;">
                         </div>
                         <div class="doc-box-content equal">
-                            <h4>Remote trainings</h4>
-                            <p>Eramba every year - a partner portal will show your company logo and website.</p>
+                            <h4>Remote trainings (Community Users)</h4>
+                            <p>Our community users can access our enterprise trainings for a fix price. Review our training schedulle here.</p>
+							<ul>
+								<li>Starting at $500</li>
+								<li>Remote</li>
+							</ul>
                         </div>
                     </div>
                 </div>
@@ -84,7 +82,10 @@ get_header();
                         </div>
                         <div class="doc-box-content equal">
                             <h4>Import Data</h4>
-                            <p>Over 35 thousand people visits eramba every year - a partner portal will show your company logo and website.</p>
+                            <p>We take your spreadsheets and do whatever is necesary to get them uploaded into eramba using CSV, APIs or crude SQL.</p>
+							<ul>
+								<li>Remote</li>
+							</ul>
                         </div>
                     </div>
                 </div>
@@ -98,7 +99,11 @@ get_header();
                         </div>
                         <div class="doc-box-content equal">
                             <h4>Complience Management</h4>
-                            <p>Over 35 thousand people visits eramba every year - a partner portal will show your company.</p>
+                            <p>We help you assess and upload your controls and policies to eramba and map them with PCI, ISO or any other compliance requirement.</p>
+							<ul>
+								<li>Starting at $500</li>
+								<li>Remote</li>
+							</ul>
                         </div>
                     </div>
                 </div>
@@ -110,7 +115,10 @@ get_header();
                         </div>
                         <div class="doc-box-content equal">
                             <h4>Risk Management</h4>
-                            <p>Every year - a partner portal will show your company logo and website.</p>
+                            <p>We train your team on risk and help you assess organisational risks, policies and controls to upload them to eramba.</p>
+							<ul>
+								<li>Starting at $500</li>
+							</ul>
                         </div>
                     </div>
                 </div>
@@ -121,8 +129,11 @@ get_header();
                             <img src="<?php echo do_shortcode('[img]'); ?>enterprise-icons/data-flow.png" alt="" style="margin-top:5px;">
                         </div>
                         <div class="doc-box-content equal">
-                            <h4>Data Flow Analisis</h4>
-                            <p>Visits eramba every year - a partner portal will show your company logo and website.</p>
+                            <h4>Data Flow Analisis (GDPR)</h4>
+                            <p>We help you assess your key assets, document how they flow, who touches them, what policies and controls exist (or not) and upload all to eramba.</p>
+							<ul>
+								<li>Onsite</li>
+							</ul>
                         </div>
                     </div>
                 </div>
@@ -136,7 +147,11 @@ get_header();
                         </div>
                         <div class="doc-box-content equal">
                             <h4>Policy Reviews</h4>
-                            <p>Over 35 thousand people visits eramba every year - a partner portal will show your company logo and website.</p>
+                            <p>Forget about reviewing policies, we do it for you remotely and at a fix price. Never again miss a policy review! (Download)</p>
+							<ul>
+								<li>Onsite</li>
+								<li>Remote</li>
+							</ul>
                         </div>
                     </div>
                 </div>
@@ -148,7 +163,11 @@ get_header();
                         </div>
                         <div class="doc-box-content equal">
                             <h4>Control Testing</h4>
-                            <p>Over 35 thousand people visits eramba every year - a partner portal will show your company logo and website.</p>
+                            <p>Forget about control testing, at a fix price we help you ensure you never miss testing again.</p>
+							<ul>
+								<li>Starting at $500</li>
+								<li>Onsite</li>
+							</ul>
                         </div>
                     </div>
                 </div>
@@ -160,12 +179,34 @@ get_header();
                         </div>
                         <div class="doc-box-content equal">
                             <h4>Vendor Assesments</h4>
-                            <p>Over 35 thousand people visits eramba every year - a partner portal will show your company.</p>
+                            <p>We'll run all your suppliers and business partners trough a Risk assessment and report you their compliance maturity.</p>
+							<ul>
+								<li>Starting at $500</li>
+								<li>Remote</li>
+							</ul>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+		<div class="col-sm-12 consulting-services-bottom-box margin-top-40">
+			<div class="row">
+				<div class="col-sm-4 col-sm-offset-4">
+					<div class="doc-box">
+						<div class="doc-box-img">
+							<img src="<?php echo do_shortcode('[img]'); ?>enterprise-icons/upgrades.png" alt="" style="margin-top:5px;">
+						</div>
+						<div class="doc-box-content equal">
+							<h4>Remote CISO</h4>
+							<p>We understand startups, we are one. We help you build and operate a professional GRC department remotely and at a fix price.</p>
+							<ul>
+								<li>Remote</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 
     <div class="text-center margin-top-30 margin-bottom-60">
@@ -178,7 +219,6 @@ get_header();
         function alignCols() {
             $('.align-row').each(function() {
                 var height = 0;
-                console.log(height);
                 $(this).find('.align-col').each(function() {
                     if ($(this).height() > height) {
                         height = $(this).height();
