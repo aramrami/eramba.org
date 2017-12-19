9edd78d99078250f35444502ad581645e377c055
Martin Marko <martin.marko@ideapp.sk>
2017-12-19 14:42:00 +0100

    Online training template

---
+++ wp-content/themes/e.org/page-templates/online-training.php
@@ -0,0 +1,247 @@
+<?php // Template Name: Online Training ?>
+<?php
+get_header();
+?>
+
+<div class="jumbo-box light-blue">
+	<div class="container">
+		<div class="row align-row vertical-align-row">
+			<div class="col-sm-7">
+                <h3 class="margin-top-10">
+                    <strong>
+                        Get help from the core team
+                    </strong>
+                </h3>
+				<p class="partnerships-text">
+                    We offer a packaged yearly support service that includes installation,
+                    training, immediate patch resolutions and features not necessarily published
+                    on the community version at extremely affordable prices. With the help of
+                    our global partners, we also provide customisation and consulting services.
+				</p>
+                <p class="partnerships-text small">
+                    The income generated from these services is what keeps this project alive.
+                    We re-invest %100 into the project and we have no intention to become yet another
+                    GRC company.
+                </p>
+			</div>
+			<div class="col-sm-5 text-center img-wrapper">
+				<img src="<?php echo do_shortcode('[img]'); ?>blackboard.png" alt="">
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container padding-top-30">
+	<div class="row consulting-services-row">
+        <div class="col-sm-12 consulting-services-top-box">
+            <div class="row align-row">
+                <div class="col-sm-12">
+                    <div class="doc-box align-col">
+        				<div class="doc-box-img">
+        					<img src="<?php echo do_shortcode('[img]'); ?>enterprise-icons/install.png" alt="" style="margin-top:5px;">
+        				</div>
+        				<div class="doc-box-content equal">
+        					<h4><a href="">Install Services (Community Users)</a></h4>
+        					<p>For our community users we help you install eramba on your company or at the cloud for a fix price.</p>
+							<ul>
+								<li>Starting at $500</li>
+								<li>Onsite</li>
+								<li>Remote</li>
+							</ul>
+        				</div>
+        			</div>
+                </div>
+            </div>
+
+            <div class="row align-row">
+                <div class="col-sm-4">
+                    <div class="doc-box align-col">
+                        <div class="doc-box-img">
+                            <img src="<?php echo do_shortcode('[img]'); ?>enterprise-icons/training.png" alt="" style="margin-top:5px;">
+                        </div>
+                        <div class="doc-box-content equal">
+                            <h4>On site trainings</h4>
+                            <p>If your team is larger than a couple of people we can travel onsite and deliver a in deep three day training.</p>
+							<ul>
+								<li>Onsite</li>
+							</ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-4">
+                    <div class="doc-box align-col">
+                        <div class="doc-box-img">
+                            <img src="<?php echo do_shortcode('[img]'); ?>enterprise-icons/training.png" alt="" style="margin-top:5px;">
+                        </div>
+                        <div class="doc-box-content equal">
+                            <h4>Remote trainings (Community Users)</h4>
+                            <p>Our community users can access our enterprise trainings for a fix price. Review our training schedulle here.</p>
+							<ul>
+								<li>Starting at $500</li>
+								<li>Remote</li>
+							</ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-4">
+                    <div class="doc-box align-col">
+                        <div class="doc-box-img">
+                            <img src="<?php echo do_shortcode('[img]'); ?>enterprise-icons/import.png" alt="" style="margin-top:5px;">
+                        </div>
+                        <div class="doc-box-content equal">
+                            <h4>Import Data</h4>
+                            <p>We take your spreadsheets and do whatever is necesary to get them uploaded into eramba using CSV, APIs or crude SQL.</p>
+							<ul>
+								<li>Remote</li>
+							</ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row align-row">
+                <div class="col-sm-4">
+                    <div class="doc-box align-col">
+                        <div class="doc-box-img">
+                            <img src="<?php echo do_shortcode('[img]'); ?>doc/calendar.png" alt="" style="margin-top:5px;">
+                        </div>
+                        <div class="doc-box-content equal">
+                            <h4>Complience Management</h4>
+                            <p>We help you assess and upload your controls and policies to eramba and map them with PCI, ISO or any other compliance requirement.</p>
+							<ul>
+								<li>Starting at $500</li>
+								<li>Remote</li>
+							</ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-4">
+                    <div class="doc-box align-col">
+                        <div class="doc-box-img">
+                            <img src="<?php echo do_shortcode('[img]'); ?>doc/risk.png" alt="" style="margin-top:5px;">
+                        </div>
+                        <div class="doc-box-content equal">
+                            <h4>Risk Management</h4>
+                            <p>We train your team on risk and help you assess organisational risks, policies and controls to upload them to eramba.</p>
+							<ul>
+								<li>Starting at $500</li>
+							</ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-4">
+                    <div class="doc-box align-col">
+                        <div class="doc-box-img">
+                            <img src="<?php echo do_shortcode('[img]'); ?>enterprise-icons/data-flow.png" alt="" style="margin-top:5px;">
+                        </div>
+                        <div class="doc-box-content equal">
+                            <h4>Data Flow Analisis (GDPR)</h4>
+                            <p>We help you assess your key assets, document how they flow, who touches them, what policies and controls exist (or not) and upload all to eramba.</p>
+							<ul>
+								<li>Onsite</li>
+							</ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row align-row no-bottom-margin">
+                <div class="col-sm-4">
+                    <div class="doc-box align-col">
+                        <div class="doc-box-img">
+                            <img src="<?php echo do_shortcode('[img]'); ?>doc/document.png" alt="" style="margin-top:5px;">
+                        </div>
+                        <div class="doc-box-content equal">
+                            <h4>Policy Reviews</h4>
+                            <p>Forget about reviewing policies, we do it for you remotely and at a fix price. Never again miss a policy review! (Download)</p>
+							<ul>
+								<li>Onsite</li>
+								<li>Remote</li>
+							</ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-4">
+                    <div class="doc-box align-col">
+                        <div class="doc-box-img">
+                            <img src="<?php echo do_shortcode('[img]'); ?>enterprise-icons/box.png" alt="" style="margin-top:5px;">
+                        </div>
+                        <div class="doc-box-content equal">
+                            <h4>Control Testing</h4>
+                            <p>Forget about control testing, at a fix price we help you ensure you never miss testing again.</p>
+							<ul>
+								<li>Starting at $500</li>
+								<li>Onsite</li>
+							</ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-4">
+                    <div class="doc-box align-col">
+                        <div class="doc-box-img">
+                            <img src="<?php echo do_shortcode('[img]'); ?>doc/suitcase.png" alt="" style="margin-top:5px;">
+                        </div>
+                        <div class="doc-box-content equal">
+                            <h4>Vendor Assesments</h4>
+                            <p>We'll run all your suppliers and business partners trough a Risk assessment and report you their compliance maturity.</p>
+							<ul>
+								<li>Starting at $500</li>
+								<li>Remote</li>
+							</ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+		<div class="col-sm-12 consulting-services-bottom-box margin-top-40">
+			<div class="row">
+				<div class="col-sm-4 col-sm-offset-4">
+					<div class="doc-box">
+						<div class="doc-box-img">
+							<img src="<?php echo do_shortcode('[img]'); ?>enterprise-icons/upgrades.png" alt="" style="margin-top:5px;">
+						</div>
+						<div class="doc-box-content equal">
+							<h4>Remote CISO</h4>
+							<p>We understand startups, we are one. We help you build and operate a professional GRC department remotely and at a fix price.</p>
+							<ul>
+								<li>Remote</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+    <div class="text-center margin-top-30 margin-bottom-60">
+		<a id="contact-redirect" href="<?php echo get_page_link(BUGS_PAGE_ID); ?>" class="btn btn-success btn-lg btn-wide">More information</a>
+	</div>
+</div>
+
+<script type="text/javascript">
+    $(function(){
+        function alignCols() {
+            $('.align-row').each(function() {
+                var height = 0;
+                $(this).find('.align-col').each(function() {
+                    if ($(this).height() > height) {
+                        height = $(this).height();
+                    }
+                });
+                $(this).find('.align-col').height(height);
+            });
+        }
+
+        $(window).on("load", function($) {
+            alignCols();
+        });
+
+    });
+</script>
+<?php get_footer(); ?>
