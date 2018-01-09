109cafbf76af2c24c6abcdded951f1a107401f10
Martin Marko <martin.marko@ideapp.sk>
2018-01-09 10:23:36 +0100

    subpage Enterprise Subscription

---
+++ wp-content/themes/e.org/page-templates/enterprise-subscription.php
@@ -0,0 +1,209 @@
+<?php // Template Name: Enterprise Subscriptions ?>
+<?php
+get_header();
+?>
+
+<div class="jumbo-box full-size light-blue">
+	<div class="container">
+		<div class="row align-row vertical-align-row">
+			<div class="col-sm-7 text-wrapper">
+                <h3 class="margin-top-10">
+                    <strong>
+                        Get help from the core team
+                    </strong>
+                </h3>
+				<p class="partnerships-text">
+                    We offer a packaged yearly support service that includes installation,
+                    training, immediate patch resolutions and features not necessarily published
+                    on the community version at extremely affordable prices. With the help of
+                    our global partners, we also provide customisation and consulting services.
+				</p>
+                <p class="partnerships-text small">
+                    The income generated from these services is what keeps this project alive.
+                    We re-invest %100 into the project and we have no intention to become yet another
+                    GRC company.
+                </p>
+			</div>
+			<div class="col-sm-5 text-center img-wrapper full-size">
+				<img src="<?php echo do_shortcode('[img]'); ?>enterprise-services.png" alt="">
+			</div>
+		</div>
+	</div>
+</div>
+<div class="container">
+    <h2 class="margin-top-40 margin-bottom-40">
+        <strong>Enterprise services</strong>
+    </h2>
+    <div class="row align-row">
+        <div class="col-sm-4">
+            <div class="doc-box doc-box-alt doc-box-partnerships doc-box-plus align-col">
+                <div class="doc-box-img">
+                    <img src="<?php echo do_shortcode('[img]'); ?>enterprise-icons/shield-colored.png" alt="">
+                </div>
+                <div class="doc-box-content">
+                    <h4>Enterprise Release</h4>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                        scelerisque eleifend sodales. Cum sociis natoque penatibus et magnis
+                        dis parturient montes, nascetur ridiculus mus.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-4">
+            <div class="doc-box doc-box-alt doc-box-partnerships doc-box-plus align-col">
+                <div class="doc-box-img">
+                    <img src="<?php echo do_shortcode('[img]'); ?>enterprise-icons/installation.png" alt="">
+                </div>
+                <div class="doc-box-content">
+                    <h4>Help to Install</h4>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                        scelerisque eleifend sodales. Cum sociis natoque penatibus et magnis
+                        dis parturient montes, nascetur ridiculus mus.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-4">
+            <div class="doc-box doc-box-alt doc-box-partnerships align-col">
+                <div class="doc-box-img">
+                    <img src="<?php echo do_shortcode('[img]'); ?>enterprise-icons/training.png" alt="">
+                </div>
+                <div class="doc-box-content">
+                    <h4>Online Trainings</h4>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                        scelerisque eleifend sodales. Cum sociis natoque penatibus et magnis
+                        dis parturient montes, nascetur ridiculus mus.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-4">
+            <div class="doc-box doc-box-alt doc-box-partnerships doc-box-plus align-col">
+                <div class="doc-box-img">
+                    <img src="<?php echo do_shortcode('[img]'); ?>enterprise-icons/support.png" alt="">
+                </div>
+                <div class="doc-box-content">
+                    <h4>Support</h4>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                        scelerisque eleifend sodales. Cum sociis natoque penatibus et magnis
+                        dis parturient montes, nascetur ridiculus mus.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-4">
+            <div class="doc-box doc-box-alt doc-box-partnerships align-col">
+                <div class="doc-box-img">
+                    <img src="<?php echo do_shortcode('[img]'); ?>enterprise-icons/upgrades.png" alt="">
+                </div>
+                <div class="doc-box-content">
+                    <h4>Software Updates</h4>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                        scelerisque eleifend sodales. Cum sociis natoque penatibus et magnis
+                        dis parturient montes, nascetur ridiculus mus.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-4">
+            <div class="doc-box doc-box-alt align-col">
+                <div class="doc-box-content">
+                    <div class="box-jumbo margin-top-40 margin-bottom-40">
+                        <strong>1800 &euro;</strong> / Year
+                    </div>
+                    <a href="#" class="btn btn-blue width-90">Get a Quote</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<hr>
+
+<div class="container">
+    <h2 class="margin-top-20 margin-bottom-40">
+        <strong>Community services</strong>
+    </h2>
+    <div class="row align-row">
+        <div class="col-sm-4">
+            <div class="doc-box doc-box-alt doc-box-partnerships align-col">
+                <div class="doc-box-img">
+                    <img src="<?php echo do_shortcode('[img]'); ?>community.png" alt="">
+                </div>
+                <div class="doc-box-content">
+                    <h4>Enterprise Release</h4>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                        scelerisque eleifend sodales. Cum sociis natoque penatibus et magnis
+                        dis parturient montes, nascetur ridiculus ociis natoque penatibus et
+                        magnis dis parturient montes.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-4">
+            <div class="doc-box doc-box-alt doc-box-partnerships align-col">
+                <div class="doc-box-img">
+                    <img src="<?php echo do_shortcode('[img]'); ?>enterprise-icons/installation.png" alt="">
+                </div>
+                <div class="doc-box-content">
+                    <h4>Help to Install</h4>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                        scelerisque eleifend sodales. Cum sociis natoque penatibus.
+                    </p>
+                    <a href="#" class="btn btn-default width-80 margin-top-10">Tell me more</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-4">
+            <div class="doc-box doc-box-alt doc-box-partnerships align-col">
+                <div class="doc-box-img">
+                    <img src="<?php echo do_shortcode('[img]'); ?>enterprise-icons/training.png" alt="">
+                </div>
+                <div class="doc-box-content">
+                    <h4>Online trainings</h4>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                        scelerisque eleifend sodales. Cum sociis natoque.
+                    </p>
+                    <a href="#" class="btn btn-default width-80 margin-top-10">Tell me more</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script type="text/javascript">
+    $(function(){
+        function alignCols() {
+            $('.align-row').each(function() {
+                var height = 0;
+                $(this).find('.align-col').each(function() {
+                    if ($(this).height() > height) {
+                        height = $(this).height();
+                    }
+                });
+                $(this).find('.align-col').height(height);
+            });
+        }
+
+        $(window).on("load", function($) {
+            alignCols();
+        });
+
+    });
+</script>
+<?php get_footer(); ?>
