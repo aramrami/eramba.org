8f43b2a6a159108916e9334f164c8936f3e5b974
Marek Melicherčík <marekmelichercik@MacBook-Air-uzivatela-Marek.local>
2018-05-25 15:37:08 +0200

    Documentation text update

--- wp-content/themes/e.org/page-templates/documentation2.php
+++ wp-content/themes/e.org/page-templates/documentation2.php
@@ -11,7 +11,7 @@ get_header();
 		<strong>Start Here</strong>
 	</h3>
 	<h4 class="doc-subtitle text-center margin-bottom-40">
-		<span>Not sure how to start? Watch this introductory video</span>
+		<span>Not sure how to start? Watch this introductory video and the follow the documentation without missing a step!</span>
 	</h4>
 
 	<div class="text-center">
@@ -42,7 +42,7 @@ get_header();
 
 <div class="container">
 	<h3 class="text-center margin-top-20">
-		<strong>Install &amp; Configuration</strong>
+		<strong>Step One - Install &amp; Configuration</strong>
 	</h3>
 	<h4 class="doc-subtitle text-center margin-bottom-40">
 		<span>Follow these guides to install and configure eramba</span>
@@ -55,11 +55,11 @@ get_header();
 					<img src="<?php echo do_shortcode('[img]'); ?>icons/download-circle.png" alt="">
 				</div>
 				<div class="doc-box-content">
-					<h4>Install</h4>
-					<p>Once you download eramba use this guide to install eramba.</p>
+					<h4>Install from Source Code</h4>
+					<p>Download our code and install the application on your Linux system using the source code.</p>
 					<ul>
 						<li><a href="https://docs.google.com/document/d/1vxh1knFcB6_ZVPUF4hy9i5XafIR3BijoZffCL_vjRVU/edit">Doc</a></li>
-						<li><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/0QALe2RX-yI?enablejsapi=1&wmode=opaque">Videos</a></li>
+						<li><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/0QALe2RX-yI?enablejsapi=1&wmode=opaque">Video</a></li>
 					</ul>
 				</div>
 			</div>
@@ -71,7 +71,7 @@ get_header();
 					<img src="<?php echo do_shortcode('[img]'); ?>icons/download-computer.png" alt="">
 				</div>
 				<div class="doc-box-content">
-					<h4>Virtual Machine</h4>
+					<h4>Pre-Installed Virtual Machine</h4>
 					<p>If you are not interested in installing eramba you can use our VMs</p>
 					<ul>
 						<li><a href="https://docs.google.com/document/d/140b6lGd3QpFvZjT927KFDz_Yd62Jjb84-PXMaioHlSw/edit#">Doc</a></li>
@@ -99,11 +99,11 @@ get_header();
 					<img src="<?php echo do_shortcode('[img]'); ?>enterprise-icons/engine.png" alt="">
 				</div>
 				<div class="doc-box-content">
-					<h4>Configure</h4>
-					<p>Follow our configuration guides in detail to make sure the system works well.</p>
+					<h4>Configuration & Access Management</h4>
+					<p>Setup email, ldap, groups and access lists for each portal.</p>
 					<ul>
 						<li><a href="https://docs.google.com/document/d/1D39uXCRfbTMvyVMFsGRyIcob8cSccAn1NWIdREodpms/edit#">Doc</a></li>
-						<li><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/a1KFzgFZ9Hs?enablejsapi=1&wmode=opaque">Videos</a></li>
+						<li><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/a1KFzgFZ9Hs?enablejsapi=1&wmode=opaque">Video</a></li>
 					</ul>
 				</div>
 			</div>
@@ -162,7 +162,7 @@ get_header();
 
 <div id="features" class="container">
 	<h3 class="text-center margin-top-20">
-		<strong>Basic features and concepts</strong>
+		<strong>Step Two - Learn the Basics</strong>
 	</h3>
 	<h4 class="doc-subtitle text-center margin-bottom-40">
 		<span>These guides cover basic features and concepts used across the system, is really important you get familiarised with them before you start using eramba!</span>
@@ -181,7 +181,7 @@ get_header();
 						<p>Search, export and report over email data from eramba.</p>
 						<ul>
 							<li><a href="https://docs.google.com/document/d/1wxeXwEz8sSm-nW9iXXx3uOR_D17tT9LgFeRBmNB4GCU/edit">Doc</a></li>
-							<li><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/o52eg9b_RoU?enablejsapi=1&wmode=opaque">Videos</a></li>
+							<li><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/o52eg9b_RoU?enablejsapi=1&wmode=opaque">Video</a></li>
 						</ul>
 					</div>
 				</div>
@@ -204,7 +204,7 @@ get_header();
 						<p>Automate email notifications, reports, reminders and more.</p>
 						<ul>
 							<li><a href="https://docs.google.com/document/d/1WraftiquqtQ7csmppaZgTuptg3rHYONO5DkwbpOLFyo/edit">Doc</a></li>
-							<li><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/fVBQscefrYQ?enablejsapi=1&wmode=opaque">Videos</a></li>
+							<li><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/fVBQscefrYQ?enablejsapi=1&wmode=opaque">Video</a></li>
 						</ul>
 					</div>
 				</div>
@@ -226,7 +226,7 @@ get_header();
 						<p>Keep track of changes (and restore anytime) on every control, risk or object.</p>
 						<ul>
 							<li><a href="https://docs.google.com/document/d/1N8ilqf1DwFR6cLNVogO1lsyQH1OBq_EufvSbAkeRlu4/edit#">Doc</a></li>
-							<li><a class="fancybox fancybox.iframe" href="https://www.youtube.com/embed/-2t2IouUQQE?enablejsapi=1&wmode=opaque">Videos</a></li>
+							<li><a class="fancybox fancybox.iframe" href="https://www.youtube.com/embed/-2t2IouUQQE?enablejsapi=1&wmode=opaque">Video</a></li>
 						</ul>
 					</div>
 				</div>
@@ -242,7 +242,7 @@ get_header();
 						<p>eramba users will only see stuff that relates to them!.</p>
 						<ul>
 							<li><a href="https://docs.google.com/document/d/1HZrWd-OnRAE4Q7XhFwHnDF_UrycYrql3pDIdHKophik/edit#">Doc</a></li>
-							<li><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/ND6qkQG8bSA?enablejsapi=1&wmode=opaque">Videos</a></li>
+							<li><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/ND6qkQG8bSA?enablejsapi=1&wmode=opaque">Video</a></li>
 
 						</ul>
 					</div>
@@ -306,7 +306,7 @@ get_header();
 						<p>Import controls, policies and more using preformated CSV files.</p>
 						<ul>
 							<li><a href="https://docs.google.com/document/d/1A7xG-jf_4lDcS8OCe8OX_0INip3yIbucYkPUbkOQgOg/edit">Doc</a></li>
-							<li><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/PB7D_OCZUnQ?enablejsapi=1&wmode=opaque">Videos</a></li>
+							<li><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/PB7D_OCZUnQ?enablejsapi=1&wmode=opaque">Video</a></li>
 						</ul>
 					</div>
 				</div>
@@ -329,7 +329,7 @@ get_header();
 						<p>Include custom fields on any of our forms (while creating a risk, control, etc).</p>
 						<ul>
 							<li><a href="https://docs.google.com/document/d/1QxaBV1WnqkBi-VVujrWVMAaLkdpU7oO0emtjUasIV0Y/edit">Doc</a></li>
-							<li><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/BiRufl5OJLg?enablejsapi=1&wmode=opaque">Videos</a></li>
+							<li><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/BiRufl5OJLg?enablejsapi=1&wmode=opaque">Video</a></li>
 						</ul>
 					</div>
 				</div>
@@ -343,10 +343,10 @@ get_header();
 
 <div id="core-functions" class="container">
 	<h3 class="text-center margin-top-20">
-		<strong>Core Functionalities</strong>
+		<strong>Step Three - Core Functionalities</strong>
 	</h3>
 	<h4 class="doc-subtitle text-center margin-bottom-40">
-		<span>Make sure you start with our controls, policies and exceptions guides before you attempt Risk or Compliance!</span>
+		<span>Follow every step of the guide to ensure you learn how eramba works!</span>
 	</h4>
 	
 	<div>
@@ -362,7 +362,7 @@ get_header();
 						<p>Document your policies, ensure they get review, publish them on a single portal, Etc.</p>
 						<ul>
 							<li><a href="https://docs.google.com/document/d/1A4drPS0cMBPyj-eL1KuhzsA3slVnaUa3fZ7KvNMRMc4/edit?usp=sharing">Doc</a></li>
-							<li><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/A3NptyqM3hY?enablejsapi=1&wmode=opaque">Videos</a></li>
+							<li><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/A3NptyqM3hY?enablejsapi=1&wmode=opaque">Video</a></li>
 						</ul>
 					</div>
 				</div>
@@ -384,7 +384,7 @@ get_header();
 						<p>Register your internal controls, their internal audits, where they are used.</p>
 						<ul>
 							<li><a href="https://docs.google.com/document/d/10Hq0HTCT_4NYXw3I1EJBfWI4OaWTBqlXZqDrQCg99E0/edit">Doc</a> </li>
-							<li><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/zihFlcsFmPw?enablejsapi=1&wmode=opaque">Videos</a></li>
+							<li><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/zihFlcsFmPw?enablejsapi=1&wmode=opaque">Video</a></li>
 						</ul>
 					</div>
 				</div>
@@ -429,10 +429,10 @@ get_header();
 					</div>
 					<div class="doc-box-content">
 						<h4>Data Flow Analysis<br>EU GDPR</h4>
-						<p>Identify your assets and analyse what controls, policies, Risks, owners and more apply to them on each stage of their lifecycle.</p>
+						<p>Document each data flow, their controls, policies and people involved.</p>
 						<ul>
 							<li><a href="https://docs.google.com/document/d/1Srt4a2vfjuEVfGONvxjd5_yO-ojoeLkLcLTsqc7v2NM/edit">Doc</a> </li>
-							<li><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/k0hAZ6Y8TTM?enablejsapi=1&wmode=opaque">Videos</a></li>
+							<li><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/k0hAZ6Y8TTM?enablejsapi=1&wmode=opaque">Video</a></li>
 						</ul>
 					</div>
 				</div>
@@ -451,10 +451,10 @@ get_header();
 					</div>
 					<div class="doc-box-content">
 						<h4>Risk Management</h4>
-						<p>Simplify Risk Management and its reviews to ensure it brings real value to your organisation. Make sure you have read our controls and policies guides before you start here!</.p>
+						<p>Simplify Risk Management and its reviews to ensure it brings real value to your organisation.</.p>
 						<ul>
 							<li><a href="https://docs.google.com/document/d/1sGaUiS6fR_oYun6mt7FktSQOdOJ0huXshMBBMOr_4N0/edit?usp=sharing">Doc</a></li>
-							<li><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/aIaY8_h_n5g?enablejsapi=1&wmode=opaque">Videos</a></li>
+							<li><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/aIaY8_h_n5g?enablejsapi=1&wmode=opaque">Video</a></li>
 						</ul>
 					</div>
 				</div>
@@ -473,10 +473,10 @@ get_header();
 					</div>
 					<div class="doc-box-content">
 						<h4>Compliance Management</h4>
-						<p>NIST, ISO, PCI - link controls and policies to your compliance requirements. Make sure you have read our controls and policies guides before you start here!</p>
+						<p>NIST, ISO, PCI - link controls and policies to your compliance requirements.</p>
 						<ul>
 							<li><a href="https://docs.google.com/document/d/1Rk85gMkTbkpud2LKk7jg2agBlwaSH2yOq6y9Z3KNzXo/edit">Doc</a> </li>
-							<li><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/rnu9-wZQels?enablejsapi=1&wmode=opaque">Videos</a></li>
+							<li><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/rnu9-wZQels?enablejsapi=1&wmode=opaque">Video</a></li>
 						</ul>
 					</div>
 				</div>
@@ -533,7 +533,7 @@ get_header();
 					<p>Create multiple, Active Directory related awareness trainings with videos, multiple choices and more.</p>
 					<ul>
 						<li><a href="https://docs.google.com/document/d/1ktGs1MasrS7FL9GUOV6TpxiLfLlQgOxe4hSKkbFMqWE/edit">Doc</a> </li>
-						<li><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/POuSzuFxa_w?enablejsapi=1&wmode=opaque">Videos</a></li>
+						<li><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/POuSzuFxa_w?enablejsapi=1&wmode=opaque">Video</a></li>
 					</ul>
 				</div>
 			</div>
@@ -550,7 +550,7 @@ get_header();
 					<p>Upload your questions and send them out so your suppliers can log in remotely and provide feedback.</p>
 					<ul>
 						<li><a href="https://docs.google.com/document/d/1pT25V1lQdc7LPjDfmal0fctT1NY72-f2pMbi7UvaSok/edit?usp=sharing">Doc</a></li>
-						<li><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/eB5_Eue03uw?enablejsapi=1&wmode=opaque">Videos</a></li>
+						<li><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/eB5_Eue03uw?enablejsapi=1&wmode=opaque">Video</a></li>
 					</ul>
 				</div>
 			</div>
@@ -566,7 +566,7 @@ get_header();
 					<p>Automate the process of reviewing user and roles accounts.</p>
 					<ul>
 						<li><a href="https://docs.google.com/document/d/1yqpdu631gkBuea1KDiUm4HDpYC6P8p3WiYfV1NpmTlA/edit?usp=sharing">Doc</a></li>
-						<li><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/Z3eNMmNbGcg?enablejsapi=1&wmode=opaque">Videos</a></li>
+						<li><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/Z3eNMmNbGcg?enablejsapi=1&wmode=opaque">Video</a></li>
 					</ul>
 				</div>
 			</div>
@@ -635,7 +635,7 @@ get_header();
 					<p>Ready to import controls and polices for Compliance Management.</p>
 					<ul>
 						<li><a href="https://docs.google.com/document/d/1UCA-e7_NSYEmsxdyODUOU7k5b_YGeByx0edZPpNQYJ0/edit#/edit?usp=sharing">Doc</a></li>
-						<li><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/v70JqOdQNg0?enablejsapi=1&wmode=opaque">Videos</a></li>
+						<li><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/v70JqOdQNg0?enablejsapi=1&wmode=opaque">Video</a></li>
 					</ul>
 					<!-- <p></p>
 					<ul>
